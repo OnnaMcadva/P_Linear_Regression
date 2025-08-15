dc33d2efdcb734743aecf437f29cb5e093eca6d6
Anna Valerjevna Makarova <anmakaro@c1r9s3.42prague.com>
2025-08-15 13:04:31 +0200

    -h

--- train.py
+++ train.py
@@ -12,9 +12,14 @@ def load_data(filename):
     mileage = []
     price = []
     with open(filename, "r") as f: # "r" reading only / with гарантирует корректное закрытие файла
-        reader = csv.DictReader(f) # ob'ect
+        reader = csv.DictReader(f) # *helpers.txt
         for row in reader:
-            print(row['mileage'], row['price'])
-            mileage.append(float(row["mileage"]))
+            print(row['km'], row['price'])
+            mileage.append(float(row["km"]))
             price.append(float(row["price"]))
     return mileage, price
+
+def normalize_feature(values):
+    mean_val = sum(values) / len(values)
+    range_val = max(values) - min(values)
+    normalized = []

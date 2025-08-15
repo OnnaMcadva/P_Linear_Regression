823b502f9c019d84f50887bf87d5a73e5ea5a95c
Anna Valerjevna Makarova <anmakaro@c3r6s2.42prague.com>
2025-08-15 13:57:52 +0200

    -h

--- train.py
+++ train.py
@@ -22,4 +22,32 @@ def load_data(filename):
 def normalize_feature(values):
     mean_val = sum(values) / len(values)
     range_val = max(values) - min(values)
-    normalized = []
+    normalized = [(v - mean_val) / range_val for v in values]
+    return normalized, mean_val, range_val
+
+def estimate_price(mileage, theta0, theta1):
+    return theta0 + theta1 * mileage
+
+def train(mileage, price, learning_rate, iterations):
+    m = len(mileage)
+    theta0 = 0.0
+    theta1 = 0.0
+
+    for _ in range(iterations):
+        sum_error_theta0 = 0.0
+        sum_error_theta1 = 0.0
+
+        for i in range(m):
+            prediction = estimate_price(mileage[i], theta0, theta1)
+            error = prediction - price[i]
+            sum_error_theta0 += error
+            sum_error_theta1 += error * mileage[i]
+
+        tmp_theta0 = theta0 - (learning_rate * (sum_error_theta0 / m))
+        tmp_theta1 = theta1 - (learning_rate * (sum_error_theta1 / m))
+
+        theta0, theta1 = tmp_theta0, tmp_theta1
+
+    return theta0, theta1
+
+def save_model()
